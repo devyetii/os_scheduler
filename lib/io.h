b58cca40970be48489604a39b0bf6a2620fd76bf
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2021-01-04 08:06:28 +0000

    Add basic IO functions needed

--- lib/io.h
+++ lib/io.h
@@ -1,18 +1,42 @@
 #pragma once
 #include <stdlib.h>
 #include <stdio.h>
+
 #include "data_structures.h"
 
-FILE* openFile(char* file_name, char permission) {
-    printf("openFile is not implemented yet\n");
+#define LINESZ 20
+
+FILE* openFile(char* path, char* perms) {
+    FILE* fptr = fopen(path, perms);
+    if (fptr == NULL) {
+        perror("Error in fopen: ");
+        exit(-1);
+    }
+
+    return fptr;
 }
 
-void closeFile(FILE* f) {
-    printf("closeFile is not implemented yet\n");
+void closeFile(FILE* fptr) {
+    fclose(fptr);
 }
+
 
 ProcessData* readProcess(FILE* f) {
-    printf("readProcess is not implemented yet\n");    
+    size_t size = LINESZ;
+    char* line = (char*) malloc(LINESZ);
+
+    // Read line and ignore comments 
+    do
+    {
+        getline(&line, &size, f);
+    } while ((line[0] != '#') && !feof(f));
+    
+    int pid, arr, runn, prior;
+    sscanf(line, "%d\t%d\t%d\t%d\n", &pid, &arr, &runn, &prior);
+    ProcessData* pd = ProcessData__create(pid, arr, runn, prior);
+    free(line);
+
+    return pd;
 }
 
 void writeProcess(FILE* f, PCB* p, int timestep) {
@@ -24,5 +48,5 @@ void writeStats(FILE* f, char* stat_name, char* stat_value) {
 }
 
 bool isEndOfFile(FILE* f) {
-    printf("isEndOfFile is not implemented yet\n");
+    return feof(f);
 }
